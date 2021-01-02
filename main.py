cee0636ef641c58e8cb0b589b6e2158f65d6ab64
Ahmad Imran <44946742+ai-rafique@users.noreply.github.com>
2021-01-02 19:23:41 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from data import data
 from art import logo,vs
 import random
 
-
 def compare(ent1,ent2):
     if ent1>ent2:
-        return 'a' 
+        return 'a'
     else:
         return 'b'
 
 while input('Would you like to play the guessing game ? (y or n) :') =='y':
+    random.shuffle(data)
     entity_1,entity_2 = random.choice(data[1::2]),random.choice(data[0::2])
     print('Who has more followers ?\n')
     print(f"{entity_1['name']} {vs} {entity_2['name']}")
@@ -21,8 +21,6 @@ while input('Would you like to play the guessing game ? (y or n) :') =='y':
         print("Sorry Wrong answer")
         print(f"{entity_1['name']} has {entity_1['follower_count']} followers")
         print(f"{entity_2['name']} has {entity_2['follower_count']} followers")    
-
-
 
 
     
